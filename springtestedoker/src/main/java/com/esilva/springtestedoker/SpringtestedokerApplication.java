aee0247ceaaf41bd8f22fa01035963b5042cbf96
ebalbinodasilva <ebalbinodasilva@gmail.com>
2021-08-03 11:01:17 -0300

    create project first commit

---
+++ springtestedoker/src/main/java/com/esilva/springtestedoker/SpringtestedokerApplication.java
@@ -0,0 +1,13 @@
+package com.esilva.springtestedoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestedokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestedokerApplication.class, args);
+	}
+
+}
