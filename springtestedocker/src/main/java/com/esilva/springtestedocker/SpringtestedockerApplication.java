7aeff243a2994b1ed6ed50742c8c0acc2ee0aefe
ebalbinodasilva <ebalbinodasilva@gmail.com>
2021-08-03 12:23:00 -0300

    CHANGE NAME PROJECT

---
+++ springtestedocker/src/main/java/com/esilva/springtestedocker/SpringtestedockerApplication.java
@@ -0,0 +1,13 @@
+package com.esilva.springtestedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestedockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestedockerApplication.class, args);
+	}
+
+}
---
+++ springtestedocker/src/test/java/com/esilva/springtestedocker/SpringtestedockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esilva.springtestedocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestedockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
